1a498ca522a47d25697e6f302e7d99beea21ae0b
Kevin Phillips <phillipskevin386@gmail.com>
2024-01-11 00:43:32 -0500

    add missing db import

--- composeApp/src/iosMain/kotlin/email/kevinphillips/biblebible/cache/DriverFactory.kt
+++ composeApp/src/iosMain/kotlin/email/kevinphillips/biblebible/cache/DriverFactory.kt
@@ -2,6 +2,7 @@ package email.kevinphillips.biblebible.cache
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import email.kevinphillips.biblebible.db.BibleBibleDatabase
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
