f23046ad8ee87edb7a00ca9962cfe778a496240d
Kevin Phillips <phillipskevin386@gmail.com>
2024-01-11 15:57:18 -0500

    selectVersesById

--- composeApp/src/commonMain/kotlin/data/apiBible/BibleAPIRepository.kt
+++ composeApp/src/commonMain/kotlin/data/apiBible/BibleAPIRepository.kt
@@ -4,6 +4,9 @@ import data.api.apiBible.BibleAPIDataModel
 import data.apiBible.json.JSON_BIBLES_API_BIBLE
 import data.apiBible.json.JSON_BOOKS_API_BIBLE
 import data.httpClient
+import email.kevinphillips.biblebible.cache.DriverFactory
+import email.kevinphillips.biblebible.db.BibleBibleDatabase
+import io.github.aakira.napier.Napier
 import io.ktor.client.call.body
 import io.ktor.client.plugins.resources.get
 import kotlinx.serialization.json.Json
@@ -41,9 +44,75 @@ suspend fun getBooksBibleAPI() {
 
 suspend fun getChapterBibleAPI() {
     try {
-        BibleAPIDataModel.chapter.value = httpClient.get(GetChapterAPIBible()).body<ChapterContent>()
+        val database = BibleBibleDatabase(driver = DriverFactory.createDriver())
+        val bibleQueries = database.bibleBibleDatabaseQueries
+            .selectVersesById(BibleAPIDataModel.selectedChapter).executeAsList()
+        val debugSampleContent = BibleAPIDataModel.chapter.value.data?.cleanedContent?.take(130)
+
+        val cachedData = bibleQueries.firstOrNull()?.let {
+            ChapterContent(
+                ChapterData(
+                    id = it.bookId,
+                    bibleId = it.bibleId,
+                    number = it.number,
+                    bookId = it.bookId,
+                    reference = it.reference,
+                    content = it.content,
+                    verseCount = it.verseCount?.toInt(),
+                    next = ChapterReference(
+                        id = it.nextId,
+                        number = it.nextNumber,
+                        bookId = it.nextBookId
+                    ),
+                    previous = ChapterReference(
+                        id = it.previousId,
+                        number = it.previousNumber,
+                        bookId = it.previousBookId
+                    ),
+                    copyright = "",
+                )
+            )
+        }
+
+
+        if (cachedData == null) {
+            Napier.d(
+                "remote api: $debugSampleContent",
+                tag = "BB2452"
+            )
+            Napier.d("start fetch", tag = "BB2452")
+            BibleAPIDataModel.chapter.value = httpClient.get(GetChapterAPIBible()).body<ChapterContent>()
+            Napier.d("end fetch", tag = "BB2452")
+            Napier.d("start delay", tag = "BB2452")
+//            delay(3000)
+            Napier.d("end delay", tag = "BB2452")
+            Napier.d("start insertVerse", tag = "BB2452")
+            BibleAPIDataModel.chapter.value.let {
+                database.bibleBibleDatabaseQueries.insertVerse(
+                    id = it.data?.id ?: "",
+                    bibleId = it.data?.bibleId ?: "",
+                    number = it.data?.number ?: "",
+                    bookId = it.data?.bookId ?: "",
+                    reference = it.data?.reference ?: "",
+                    copyright = it.data?.copyright ?: "",
+                    verseCount = it.data?.verseCount?.toLong() ?: 0L,
+                    content = it.data?.cleanedContent ?: "",
+                    nextId = it.data?.next?.number ?: "",
+                    nextNumber = it.data?.next?.bookId ?: "",
+                    nextBookId = it.data?.previous?.id ?: "",
+                    previousId = it.data?.previous?.number ?: "",
+                    previousNumber = it.data?.previous?.bookId ?: "",
+                    previousBookId = it.data?.previous?.id ?: "",
+                )
+            }
+            Napier.d("end insertVerse", tag = "BB2452")
+        } else {
+            BibleAPIDataModel.chapter.value = cachedData
+            Napier.d("db query: $debugSampleContent", tag = "BB2452")
+        }
+
     } catch (e: Exception) {
-        println("Error: ${e.message}")
+        Napier.e("Error: ${e.message}", tag = "BB2452")
     } finally {
         httpClient.close()
     }
