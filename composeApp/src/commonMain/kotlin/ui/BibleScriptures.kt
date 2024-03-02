555c617af2caa439b4dce6d38cef1ccb12f84e00
Kevin Phillips <phillipskevin386@gmail.com>
2024-03-02 00:58:36 -0500

    pointer input with LazyColumn

--- composeApp/src/commonMain/kotlin/ui/BibleScriptures.kt
+++ composeApp/src/commonMain/kotlin/ui/BibleScriptures.kt
@@ -1,11 +1,9 @@
 package ui
 
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,6 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import data.bibleIQ.BibleChapterUIState
@@ -21,19 +20,28 @@ import data.bibleIQ.BibleChapterUIState
 internal fun BibleScriptures(chapters: BibleChapterUIState, scrollState: ScrollState) {
     var fontSize by remember { mutableStateOf(16f) }
     val minTextSize = 16f
-    val state = rememberTransformableState { zoomChange, _, _ ->
-        fontSize *= zoomChange
-    }
+    var scale by remember(chapters.chapterId) { mutableStateOf(1f) }
 
-    Column(modifier = Modifier.verticalScroll(scrollState)) {
+    LazyColumn(
+        modifier = Modifier
+//            .verticalScroll(scrollState)
+            .pointerInput(Unit) {
+                detectTransformGestures { _, _, zoom, _ ->
+                    scale *= zoom
+                    fontSize = (fontSize * scale) //.coerceAtLeast(minTextSize)
+                    scale = 1f
+                }
+            }
+    ) {
+
         chapters.text?.let {
-            Text(
-                text = it,
-                fontSize = fontSize.coerceAtLeast(minTextSize).sp,
-                modifier = Modifier
-                    .padding(4.dp)
-                    .transformable(state = state)
-            )
+            item {
+                Text(
+                    text = it,
+                    fontSize = fontSize.sp,
+                    modifier = Modifier.padding(4.dp)
+                )
+            }
         }
     }
 }
