a84f967ab421af32b3353ea2e2eb810c6a39a358
Kevin Phillips <phillipskevin386@gmail.com>
2024-04-18 02:24:57 -0400

    add desktop icons

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -22,6 +22,21 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "BibleBible"
             packageVersion = "1.0.0"
+
+            val iconsRoot = project.file("desktop-icons")
+            macOS {
+                iconFile.set(iconsRoot.resolve("BibleBible_ico_iv_rounded.icns"))
+            }
+            windows {
+                iconFile.set(iconsRoot.resolve("BibleBible_ico_iv_rounded.ico"))
+                menuGroup = "Bible Bible"
+                // see https://wixtoolset.org/documentation/manual/v3/howtos/general/generate_guids.html
+                upgradeUuid = "18159995-d968-4CD2-8885-77BFA97CFC7G"
+
+            }
+            linux {
+                iconFile.set(iconsRoot.resolve("BibleBible_ico_iv_rounded.png"))
+            }
         }
     }
 }
