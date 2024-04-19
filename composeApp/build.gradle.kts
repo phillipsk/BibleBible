8d1e0e0c7c7cb75092375dccf4f41c2001f19aae
Kevin Phillips <phillipskevin386@gmail.com>
2024-04-19 03:18:04 -0400

    release version 7

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -85,7 +85,7 @@ android {
         applicationId = "email.kevinphillips.biblebible"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
+        versionCode = 7
         versionName = "6.1"
     }
     packaging {
