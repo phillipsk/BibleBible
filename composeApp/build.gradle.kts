2d60ffa54fe1d2f2373f1ce718e2a07b9d8d6735
Kevin Phillips <phillipskevin386@gmail.com>
2024-04-24 14:52:53 -0400

    increment version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -85,8 +85,8 @@ android {
         applicationId = "email.kevinphillips.biblebible"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 7
-        versionName = "6.1"
+        versionCode = 8
+        versionName = "6.2"
     }
     packaging {
         resources {
