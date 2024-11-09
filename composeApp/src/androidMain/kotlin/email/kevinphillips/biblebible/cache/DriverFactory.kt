f8d2d646d43ee072f5a40e43152d81f92cd23dd8
Kevin Phillips <phillipskevin386@gmail.com>
2024-11-08 23:37:34 -0500

    suppress lint on driver static leak

--- composeApp/src/androidMain/kotlin/email/kevinphillips/biblebible/cache/DriverFactory.kt
+++ composeApp/src/androidMain/kotlin/email/kevinphillips/biblebible/cache/DriverFactory.kt
@@ -1,10 +1,12 @@
 package email.kevinphillips.biblebible.cache
 
+import android.annotation.SuppressLint
 import android.content.Context
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import email.kevinphillips.biblebible.db.BibleBibleDatabase
 
+@SuppressLint("StaticFieldLeak")
 actual object DriverFactory {
     lateinit var context: Context
     private var driver: SqlDriver? = null
